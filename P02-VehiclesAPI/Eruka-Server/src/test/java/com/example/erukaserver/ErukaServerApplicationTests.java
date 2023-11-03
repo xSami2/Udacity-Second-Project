8e68bcd6da28f8305edafa1762633e7a93fb0a44
Your Name <391108459@qu.edu.sa>
2023-11-03 21:26:58 +0300

    Config-Eruka-Server

---
+++ P02-VehiclesAPI/Eruka-Server/src/test/java/com/example/erukaserver/ErukaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.erukaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErukaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
