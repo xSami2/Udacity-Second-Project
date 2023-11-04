c3ed5bceefd7e1d077b336015c505d5eb52a7038
Your Name <391108459@qu.edu.sa>
2023-11-05 00:20:48 +0300

    Convert the Price Microservice into Spring data rest

---
+++ P02-VehiclesAPI/pricing-service/src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS PRICE (
+                                     vehicleId LONG PRIMARY KEY auto_increment,
+                                     currency VARCHAR(20),
+    price DECIMAL(6,2)
+    );
